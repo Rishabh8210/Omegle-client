fe82adf49df50eb575f2a6c9a4a77b3f8c471c26
Rishabh Pandey <rishabhpandey8092@gmail.com>
2026-03-03 00:36:54 +0530

    feat(ChatScreen): Update chat-screen ui

--- app/components/chat/ChatScreen.tsx
+++ app/components/chat/ChatScreen.tsx
@@ -1,7 +1,25 @@
+import { Send } from "lucide-react";
+
 export function ChatScreen(){
     return (
-        <div className="h-full w-[40%] bg-amber-500/25 ">
+        <div className="h-full w-[40%] flex flex-col justify-between border-l border-zinc-700/40 p-5">
+            <div className="h-[6%] w-full">
+                <select className="border rounded p-1 outline-0">
+                    <option>English</option>
+                    <option>German</option>
+                    <option>French</option>
+                    <option>Hindi</option>
+                </select>
+            </div>
 
+            <div className="h-[87%] w-full ">
+
+            </div>
+
+            <div className="h-[7%] w-full rounded border flex justify-between items-center px-5 bg-zinc-700/10">
+                <input type="text" placeholder="Type new message" name="input-msg" className="text-lg text-zinc-300/80 w-10/12 p-0 outline-0" />
+                <Send className="hover:bg-zinc-200/10 p-2 cursor-pointer rounded-lg" size={38} />
+            </div>
         </div>
     )
 }
--- app/components/chat/VideoCall.tsx
+++ app/components/chat/VideoCall.tsx
@@ -1,7 +1,12 @@
 export function VideoCall(){
     return (
-        <div className="h-full w-2/4">
+        <div className="h-full w-2/4 p-5 flex flex-col justify-evenly">
+            <div className="h-[45%] w-full bg-zinc-700 rounded-lg">
 
+            </div>
+            <div className="h-[45%] w-full bg-zinc-700 rounded-lg">
+
+            </div>
         </div>
     )
 }
