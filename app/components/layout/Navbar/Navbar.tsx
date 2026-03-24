e201904850b68edafd31153ec9dc47f8ad8b480e
Rishabh Pandey <rishabhpandey8092@gmail.com>
2026-03-24 23:29:09 +0530

    refacor(Nav): Fix large screen navbar

--- app/components/layout/Navbar/Navbar.tsx
+++ app/components/layout/Navbar/Navbar.tsx
@@ -18,10 +18,10 @@ export function Navbar() {
                         className="h-11 rounded-full w-auto"
                     />
                 </div>
-                <ul className={`${rubikIso.className} hidden md:flex w-full items-center justify-center gap-7`}>
-                    <li className={"text-lg font-semibold text-white/50 hover:text-white hover:cursor-pointer hover:underline transition-transform"}>Home</li>
-                    <li className="text-lg font-semibold text-white/50 hover:text-white hover:cursor-pointer hover:underline transition-transform">About Us</li>
-                    <li className="text-lg font-semibold text-white/50 hover:text-white hover:cursor-pointer hover:underline transition-transform">FAQ</li>
+                <ul className={`${rubikIso.className} hidden md:flex w-full items-center justify-center gap-10`}>
+                    <li className="text-lg lg:text-2xl font-semibold text-black/70 hover:text-black dark:text-white/50 dark:hover:text-white  hover:cursor-pointer hover:underline transition-transform">Home</li>
+                    <li className="text-lg lg:text-2xl font-semibold text-black/70 hover:text-black dark:text-white/50 dark:hover:text-white hover:cursor-pointer hover:underline transition-transform">About Us</li>
+                    <li className="text-lg lg:text-2xl font-semibold text-black/70 hover:text-black dark:text-white/50 dark:hover:text-white hover:cursor-pointer hover:underline transition-transform">FAQ</li>
                 </ul>
                 <div className={` md:hidden ${rubikIso.className} w-full flex justify-end`}>
                     <button onClick={() => setIsMenuOpen(prev => !prev)} className="text-4xl md:hidden font-extrabold active:rotate-180 cursor-pointer transition-all">£</button>
